4425b9b3e6db902f45c86e49b3a3bc72004fd5ad
Rahul <vemurirahul93@gmail.com>
2017-01-01 18:44:37 -0500

    Create FakeConfig.java

---
+++ extra/TestHarness/FakeConfig.java
@@ -0,0 +1,48 @@
+package TestHarness;
+
+import javax.servlet.*;
+
+import java.util.*;
+
+class Config implements ServletConfig
+{
+
+    //All these data members are initialised when createServlets(handler, context) is called
+    private String servletName;
+    private Context context;
+    private HashMap<String, String> initParams;
+
+    public Config(String servletName, Context context)
+    {
+        this.servletName = servletName;
+        this.context = context;
+        initParams = new HashMap<String, String>();
+    }
+
+    public String getInitParameter(String name)
+    {
+        return initParams.get(name);
+    }
+
+    public Enumeration getInitParameterNames()
+    {
+        Set<String> keys = initParams.keySet();
+        Vector<String> atts = new Vector<String>(keys);
+        return atts.elements();
+    }
+
+    public ServletContext getServletContext()
+    {
+        return context;
+    }
+
+    public String getServletName()
+    {
+        return servletName;
+    }
+
+    void setInitParam(String name, String value)
+    {
+        initParams.put(name, value);
+    }
+}
